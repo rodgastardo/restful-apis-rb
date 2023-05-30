693808c46b0b80a63a485916ca8f369db2ff6a2e
Rodrigo Gastardo <rodrigogastardopereira@gmail.com>
2023-05-30 11:08:18 -0300

    commit inicial

---
+++ config/routes.rb
@@ -0,0 +1,42 @@
+Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
+  
+  #resources :auths, only: [:create]
+  resources :kinds
+  
+    api_version(:module => "V1", :path => {:value => "v1"}) do
+  resources :contacts do
+    resource :kind, only: [:show]
+    resource :kind, only: [:show], path: 'relationships/kind'
+
+    resource :phones, only: [:show]
+    resource :phones, only: [:show], path: 'relationships/phones'
+
+    resource :phone, only: [:update, :create, :destroy]
+    resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+
+    resource :address, only: [:show, :update, :create, :destroy]
+    resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    end
+  end
+
+  api_version(:module => "V2", :path => {:value => "v2"}) do
+    resources :contacts do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
+
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
+
+      resource :phone, only: [:update, :create, :destroy]
+      resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+  end
+end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
